b9d7c948c3c63409c4fc01051fbfb356d99dcb1b
Ram Vishwakarma <rv7029919@gmail.com>
2026-07-24 11:00:13 +0530

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://srms-backend-v3qc.onrender.com',
         changeOrigin: true,
       },
     },
